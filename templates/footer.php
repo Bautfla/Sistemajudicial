d771740c6edc049ae3cfde229c9f6de551d78578
Bautfla <bauti.perez.antigues@gmail.com>
2026-07-01 20:29:00 -0300

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,5 @@
-</div> <footer style="text-align: center; margin-top: 3rem; color: #777;">
-    <p>&copy; <?php echo date('Y'); ?> Sistema de Judicial Web</p>
-</footer>
+<footer style="text-align: center; color: #7f8c8d; padding: 1rem; font-size: 0.9rem;">
+        <p>&copy; <?php echo date("Y"); ?> Sistema de Gestión Judicial - 7° 6ta Programación</p>
+    </footer>
 </body>
 </html>
